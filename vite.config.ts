3ba2694ad9e21243d25ee59c7afe51db35ca126c
hariszaka1 <hariszaka2@gmail.com>
2025-08-02 17:56:16 +0700

    Initial deploy Rubik's Timer

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// Ganti sesuai dengan nama repo GitHub kamu
+const repoName = 'Rubik-s-Timer-by-Blitarian-Speedcuber-Brotherhood';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    base: `/${repoName}/`, // <--- penting untuk GitHub Pages
+    plugins: [react()],     // <--- penting untuk React support
+
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'), // disarankan alias ke /src
       }
-    };
+    }
+  };
 });
